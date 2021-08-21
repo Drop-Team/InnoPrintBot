aa6bd9a00877f56651affc61f4519202f6d1446a
blinikar <blinov.egor2013@mail.ru>
2021-08-21 20:13:55 +0300

    0.3

--- printerbot.py
+++ printerbot.py
@@ -13,67 +13,107 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-import os
+#TODO list
+"""
+Autoremoving files
+"""
+
+from datetime import datetime
+import subprocess
 import logging
+
+import emailconfirmation
+import config
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-max_file_size = 64 * 1024 * 1024 * 8
+max_file_size = 64 * 1024 * 1024
+
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO,
+    #filename='bot.log'
 )
 
 logger = logging.getLogger(__name__)
 
 def send_to_printer(file_path):
-    os.system(f'lp {file_path}')
+    #subprocess.run(['lp', file_path])
+    print('tipo pechataet')
+    logger.info(f'Document {file_path} had sent to printer')
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\! This is @innoprintbot you can send the file to print some documents. We recommend to use PDF format',
-        reply_markup=ForceReply(selective=True),
-    )
+    update.message.reply_text(fr'Hi\! This is @innoprintbot you can print some document with my help. We recommend to use PDF format. Send \'/auth <your_email>\' to confirm that you are student or stuff')
+    logger.info(f'User {user.name} started bot')
 
+def auth(update: Update, context: CallbackContext) -> None:
+    if emailconfirmation.is_authorized(update.effective_user.id):
+        update.message.reply_text('You are already authorized')
+        return
+    if len(context.args) != 1:
+        update.message.reply_text('You need to write only your innopolis email')
+        return
+    logger.info(f'User {update.effective_user.id} attempts to sign up with email {context.args[0]}')
+    emailconfirmation.send_email(update.effective_user.id, context.args[0])
+    update.message.reply_text('Send \'/code <code_from_your_email>\' to confirm your email')
+
+def code(update: Update, context: CallbackContext) -> None:
+    logger.info(f'User {update.effective_user.id} attempts to input a code')
+    if len(context.args) != 1:
+        update.message.reply_text('You need to write only your code from email')
+        return
+    user_code = context.args[0]
+    if not(emailconfirmation.sign_up(update.effective_user.id, user_code)):
+        update.message.reply_text('Wrong code')
+        return
+    update.message.reply_text('Great! Now you can just send the PDF file and print it')
+    
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('To print your documents just send your PDF file to me')
+    logger.info(f'User {update.effective_user.name} request help')
 
 def print_file(update: Update, context: CallbackContext) -> None:
-    bot = context.bot
-    update.message.reply_text('Go to the printer and take your documents :)')
-    
-    file_id = update.message.document.file_id
-    file_name = update.message.document.file_name
-    file_size = update.message.document.file_size
+    if not(emailconfirmation.is_authorized(update.effective_user.id)):
+        update.message.reply_text('You are not authorized')
+        return
 
+    bot = context.bot
+    file_id = update.message.document.file_id
+    old_file_extension = update.message.document.file_name.split('.')
+    if(len(old_file_extension) <= 1):
+        update.message.reply_text('Your file is not supported, sorry')
+        logger.info(f'User {update.effective_user.name} sent file without extention {update.message.document.file_name}, we dont save it')
+        return
+    file_name = datetime.utcnow().strftime('%Y%m%d-%H%M%S%f' + old_file_extension[len(old_file_extension)-1])
+    file_size = update.message.document.file_size
+    
     if file_size > max_file_size:
         update.message.reply_text('Your file is too big')
+        logger.info(f'User {update.effective_user.name} sent too big the document {update.message.document.file_name}, we dont save it')
+        return
 
     file_path = "printed_files/" + file_name
 
+    update.message.reply_text('We are downloading your file...')
     new_file = bot.get_file(file_id)
     new_file.download(file_path)
 
     send_to_printer(file_path=file_path)
-
-
-    
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text('Done! Go to the printer and take your documents :)')
+    logger.info(f'User {update.effective_user.name} sent the document {update.message.document.file_name}, we saved it as {file_name} and sent to printer')
 
 
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1980039271:HAHAHAHAHAAH")
+    updater = Updater(config.token)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -81,10 +121,13 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("auth", auth))
+    dispatcher.add_handler(CommandHandler("code", code))
     dispatcher.add_handler(MessageHandler(Filters.document, print_file))
 
     # Start the Bot
     updater.start_polling()
+    logger.info('Bot started')
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
