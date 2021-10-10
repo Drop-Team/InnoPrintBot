a8fafc34c3daf9cf7029e29cacfa37f86ecf761f
Egor <edkuz11@yandex.ru>
2021-10-10 03:35:35 +0300

    Logging & Requests update

--- bot/logger.py
+++ bot/logger.py
@@ -2,6 +2,6 @@ import logging
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO,
-    # filename="bot.log"
+    filename="bot.log"
 )
 logger = logging.getLogger(__name__)
