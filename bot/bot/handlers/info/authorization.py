90af0df628da148b46356e59ba709c38339f6602
Egor <edkuz11@yandex.ru>
2022-05-17 16:34:50 +0300

    Auth error text edited

--- bot/bot/handlers/info/authorization.py
+++ bot/bot/handlers/info/authorization.py
@@ -15,5 +15,6 @@ async def authorize_command(msg: types.Message):
 async def is_not_authorized_error(msg: types.Message):
     """Send an error message when the user is not authorized, but is required to be"""
 
-    text = "First you have to confirm that you are a student or an employee of Innopolis University.\nUse /auth"
+    text = "First you have to confirm that you are a student or an employee of Innopolis University.\n" \
+           "Use /auth command."
     await msg.answer(text)
