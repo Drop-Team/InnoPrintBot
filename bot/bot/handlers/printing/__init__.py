15f64af05e3a6c1edbc37e3a3689494d1633f481
Egor <edkuz11@yandex.ru>
2022-05-17 16:35:33 +0300

    Printing now supports images

--- bot/bot/handlers/printing/__init__.py
+++ bot/bot/handlers/printing/__init__.py
@@ -4,4 +4,4 @@ from .print import print_document
 
 
 def setup(dp: Dispatcher):
-    dp.register_message_handler(print_document, content_types=["document"], is_authorized=True)
+    dp.register_message_handler(print_document, content_types=["any"], is_authorized=True)
--- bot/bot/handlers/printing/print.py
+++ bot/bot/handlers/printing/print.py
@@ -3,17 +3,22 @@ import os
 from aiogram import types
 from aiogram.utils import exceptions
 
-from bot.utils.printing.job.job import PrintJob
-from bot.utils.printing.file import FileNameGenerator, PdfConverter
 from bot.utils.metrics import metrics
+from bot.utils.printing.file import FileNameGenerator, PdfConverter
+from bot.utils.printing.job.job import PrintJob
 
 
 async def print_document(msg: types.Message):
     """Handler for receiving documents and preparing it to print"""
 
+    if msg.document is None and not msg.photo:
+        text = "Please send the document you want to print (better in PDF format)."
+        return await msg.answer(text)
+
     status_message = await msg.answer("Downloading file...")
 
-    metrics.print_files_extensions.labels(msg.document.mime_type).inc()
+    mime_type = msg.document.mime_type if msg.document else "[image]"
+    metrics.print_files_extensions.labels(mime_type).inc()
 
     try:
         file_path = await download_file_from_message(msg)
@@ -46,6 +51,13 @@ async def download_file_from_message(msg: types.Message) -> str:
     bot = msg.bot
 
     destination = os.getenv("FILES_PATH") + FileNameGenerator.get_next_filename()
-    await bot.download_file_by_id(msg.document.file_id, destination=destination)
+
+    file_id: [str] = None
+    if msg.document is not None:
+        file_id = msg.document.file_id
+    if msg.photo:
+        file_id = msg.photo[-1].file_id
+
+    await bot.download_file_by_id(file_id, destination=destination)
 
     return destination
