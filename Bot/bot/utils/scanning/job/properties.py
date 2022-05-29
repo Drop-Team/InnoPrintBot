2f5715bb7852136e02942159102791afdfd2d50d
Egor <edkuz11@yandex.ru>
2022-05-29 23:55:45 +0300

    Added DPI label to properties

--- Bot/bot/utils/scanning/job/properties.py
+++ Bot/bot/utils/scanning/job/properties.py
@@ -21,10 +21,10 @@ class QualityProperty(Property):
     webapp_name = "quality"
 
     values_mapped = [
-        MappedValue(source="200", readable="200", webapp="quality-200"),
-        MappedValue(source="300", readable="300", webapp="quality-300"),
-        MappedValue(source="400", readable="400", webapp="quality-400"),
-        MappedValue(source="600", readable="600", webapp="quality-600"),
+        MappedValue(source="200", readable="200 DPI", webapp="quality-200"),
+        MappedValue(source="300", readable="300 DPI", webapp="quality-300"),
+        MappedValue(source="400", readable="400 DPI", webapp="quality-400"),
+        MappedValue(source="600", readable="600 DPI", webapp="quality-600"),
     ]
 
     def __init__(self):
