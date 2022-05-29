7fdd9582afc6ca5ec47027075b60306e3cc3b112
Egor <edkuz11@yandex.ru>
2022-05-29 22:41:24 +0300

    Subscription address added

--- Bot/bot/utils/printing/cups_service/subscription.py
+++ Bot/bot/utils/printing/cups_service/subscription.py
@@ -1,9 +1,10 @@
 import cups
 from cups_notify import Subscriber
+import os
 
 
 def create_subscription(conn: cups.Connection):
-    sub = Subscriber(conn)
+    sub = Subscriber(conn, local_address=os.getenv("BOT_CUPS_SUBSCRIPTION_LOCAL_ADDRESS"))
 
     return sub
 
