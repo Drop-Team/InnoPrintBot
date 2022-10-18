14fa815b681a410caa24d001695df28718906419
Egor Kuziakov <edkuz11@yandex.ru>
2022-10-18 17:13:01 +0300

    Loops fix

--- Bot/bot/loops/__init__.py
+++ Bot/bot/loops/__init__.py
@@ -1,9 +1,10 @@
 import asyncio
 
+from . import authorized_users
 from . import events
-from . import print_jobs
 from . import files_cleaner
 from . import metrics
+from . import print_jobs
 
 
 async def main_loop(seconds: float):
@@ -12,6 +13,7 @@ async def main_loop(seconds: float):
     print_jobs.loops_counter.set_required_loops_number_by_seconds(seconds, 3)
     files_cleaner.loops_counter.set_required_loops_number_by_seconds(seconds, 3)
     metrics.loops_counter.set_required_loops_number_by_seconds(seconds, 30)
+    authorized_users.loops_counter.set_required_loops_number_by_seconds(seconds, 1800)
 
     while True:
         try:
@@ -22,6 +24,8 @@ async def main_loop(seconds: float):
             await files_cleaner.check_for_files()
 
             await metrics.update_metrics()
+
+            await authorized_users.update_users()
 
         except Exception as e:
             pass
